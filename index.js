0b4ec88268d277ad4d2cf9542906e8d81319ed0b
CGeH1 <cliffton.gh2008@gmail.com>
2025-02-27 15:38:59 +0700

    fix style + all button work

--- index.js
+++ index.js
@@ -45,38 +45,18 @@ const menus = [{
 }];
 
 const cart = [
-    /// TODO: Create data structure for menu shopping cart
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0]
 ];
 
 function addQty(menuIndex, priceIndex) {
-    /// TODO: Access qty of menuIndex and priceIndex in cart, and increment by 1
-    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = qty;
+    cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex] + 1;
+    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex];
 }
 
 function substractQty(menuIndex, priceIndex) {
-    /// TODO: Access qty of menuIndex and priceIndex in cart, and decrement by 1
-    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = qty;
+    cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex] - 1;
+    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex];
 }
-let qty = 0;
-let qty2 = 0;
-
-function addQty() {
-    qty = qty+1
-    document.getElementById('qty').innerHTML = qty;
-}
-
-function substractQty() {
-    qty = qty-1
-    document.getElementById('qty').innerHTML = qty;
-}
-
-function addQty2() {
-    qty2 = qty2+1
-    document.getElementById('qty2').innerHTML = qty2;
-}
-
-function substractQty2() {
-    qty2 = qty2-1
-    document.getElementById('qty2').innerHTML = qty2;
-}
-
