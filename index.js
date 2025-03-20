014fb81c0c47e1c2b56760736e07101eb0b332bc
CGeH1 <cliffton.gh2008@gmail.com>
2025-03-20 15:11:15 +0700

    add button animation

--- index.js
+++ index.js
@@ -45,16 +45,36 @@ const menus = [{
 }];
 
 const cart = [
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0]
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
 ];
 
 let menuArea = "";
 
-for (let i=0; i< menus.length; i++) {
-    const e = menus [i]
+for (let i = 0; i < menus.length; i++) {
+    const e = menus[i];
+    let menuPrices = "";
+
+    for (let j = 0; j < e.prices.length; j++) {
+        const f = e.prices[j];
+        menuPrices += `
+            <div class="menu-price-row">
+                <span class="price-description">${f.label}</span>
+                <div class="price-and-qty">
+                    <span class="price">${f.price}</span>
+                    <button onclick="substractQty(${i},${j})">
+                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
+                    </button>
+                    <span class="qty" id="qty${i}${j}">0</span>
+                    <button onclick="addQty(${i},${j})">
+                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
+                    </button>
+                </div>
+            </div>
+        `;
+    }
 
     menuArea += `
         <div class="menu-tile">
@@ -63,149 +83,19 @@ for (let i=0; i< menus.length; i++) {
             </div>
             <div class="menu-tile-name">${e.title}</div>
             <div class="menu-tile-description">${e.description}</div>
-            <div class="menu-price-row">
-                <span class="price-description"></span>
-                <div class="price-and-qty">
-                    <span class="price">35</span>
-                    <button onclick="substractQty(0,0)">
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty00">0</span>
-                    <button onclick="addQty(0,0)">
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-
-            <div class="menu-price-row">
-                <span class="price-description">+ Grilled Salmon</span>
-                <div class="price-and-qty">
-                    <span class="price">55</span>
-                    <button onclick='substractQty(0,1)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty01">0</span>
-                    <button onclick='addQty(0,1)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <div class="menu-tile">
-            <div class="menu-photo">
-                <img src="assets/pizza.jpg" alt="pizza">
-            </div>
-            <div class="menu-tile-name">Triple Cheese Pizza</div>
-            <div class="menu-tile-description">“Indulge yourself in this simple yet delicious delicacy”</div>
-            <div class="menu-price-row">
-                <span class="price-description">1 Slice</span>
-                <div class="price-and-qty">
-                    <span class="price">15</span>
-                    <button onclick='substractQty(1,0)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty10">0</span>
-                    <button onclick='addQty(1,0)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-            <div class="menu-price-row">
-                <span class="price-description">Pan (6 slices)</span>
-                <div class="price-and-qty">
-                    <span class="price">60</span>
-                    <button onclick='substractQty(1,1)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty11">0</span>
-                    <button onclick='addQty(1,1)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <div class="menu-tile">
-            <div class="menu-photo">
-                <img src="assets/wine.jpg" alt="wine">
-            </div>
-            <div class="menu-tile-name">Seasonal Wine</div>
-            <div class="menu-tile-description">“A great meal becomes perfect when combined with a good wine”
-            </div>
-            <div class="menu-price-row">
-                <span class="price-description">Glass</span>
-                <div class="price-and-qty">
-                    <span class="price">100</span>
-                    <button onclick='substractQty(2,0)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty20">0</span>
-                    <button onclick='addQty(2,0)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-            <div class="menu-price-row">
-                <span class="price-description">Bottle</span>
-                <div class="price-and-qty">
-                    <span class="price">1500</span>
-                    <button onclick='substractQty(2,1)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty21">0</span>
-                    <button onclick='addQty(2,1)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <div class="menu-tile">
-            <div class="menu-photo">
-                <img src="assets/coffee.jpg" alt="coffee">
-            </div>
-            <div class="menu-tile-name">Coffee</div>
-            <div class="menu-tile-description">“The best coffee you can have from the land of Indonesia”
-            </div>
-            <div class="menu-price-row">
-                <span class="price-description">Americano</span>
-                <div class="price-and-qty">
-                    <span class="price">25</span>
-                    <button onclick='substractQty(3,0)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty30">0</span>
-                    <button onclick='addQty(3,0)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-            <div class="menu-price-row">
-                <span class="price-description">Latte</span>
-                <div class="price-and-qty">
-                    <span class="price">30</span>
-                    <button onclick='substractQty(3,1)'>
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty31">0</span>
-                    <button onclick='addQty(3,1)'>
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
+        ${menuPrices}
         </div>
     `
-            
-
 }
 
+document.getElementById("menu-area").innerHTML = menuArea;
+
 function addQty(menuIndex, priceIndex) {
-    cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex] + 1;
-    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex];
+    cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex] + 1;
+    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex];
 }
 
 function substractQty(menuIndex, priceIndex) {
-    cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex] - 1;
-    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart [menuIndex][priceIndex] = cart [menuIndex][priceIndex];
+    cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex] - 1;
+    document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex];
 }
