c02403ab2615b2e06b62eca3fc6a04d999557827
CGeH1 <cliffton.gh2008@gmail.com>
2025-04-24 15:58:23 +0700

    reset display after co + co button fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let totalPrice = 0
+let totalPrice = 0;
+let cart = [];
 
 const menus = [{
     title: "Avocado Pesto Salad",
@@ -46,62 +47,78 @@ const menus = [{
     }]
 }];
 
-const cart = []
-for (let i = 0; i < menus.length; i++) {
-    cart.push([0, 0])
-}
-
-let menuArea = "";
-
-for (let i = 0; i < menus.length; i++) {
-    const e = menus[i];
-    let menuPrices = "";
-
-    for (let j = 0; j < e.prices.length; j++) {
-        const f = e.prices[j];
-        menuPrices += `
-            <div class="menu-price-row">
-                <span class="price-description">${f.label}</span>
-                <div class="price-and-qty">
-                    <span class="price">${f.price}</span>
-                    <button onclick="substractQty(${i},${j})">
-                        <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
-                    </button>
-                    <span class="qty" id="qty${i}${j}">0</span>
-                    <button onclick="addQty(${i},${j})">
-                        <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
-                    </button>
-                </div>
-            </div>
-        `;
-    }
-
-    menuArea += `
-        <div class="menu-tile">
-            <div class="menu-photo">
-                <img src="${e.photoUrl}" alt="${e.title}">
-            </div>
-            <div class="menu-tile-name">${e.title}</div>
-            <div class="menu-tile-description">${e.description}</div>
-        ${menuPrices}
-        </div>
-    `
-}
-
-document.getElementById("menu-area").innerHTML = menuArea;
+reset();
 
 function addQty(menuIndex, priceIndex) {
     cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex] + 1;
-    totalPrice += menus [menuIndex].prices[priceIndex].price;
-    document.getElementById("checkout").innerHTML = "Rp. " + (totalPrice*1000).toLocaleString();
+    totalPrice += menus[menuIndex].prices[priceIndex].price;
+    document.getElementById("checkout").innerHTML = "Rp. " + (totalPrice * 1000).toLocaleString();
     document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex];
 }
 
 function substractQty(menuIndex, priceIndex) {
     if (cart[menuIndex][priceIndex] >= 1) {
         cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex] - 1;
-        totalPrice -= menus [menuIndex].prices[priceIndex].price;
-        document.getElementById("checkout").innerHTML = "Rp. " + (totalPrice*1000).toLocaleString();
+        totalPrice -= menus[menuIndex].prices[priceIndex].price;
+        document.getElementById("checkout").innerHTML = "Rp. " + (totalPrice * 1000).toLocaleString();
         document.getElementById("qty" + menuIndex + priceIndex).innerHTML = cart[menuIndex][priceIndex] = cart[menuIndex][priceIndex];
     }
+}
+
+function checkout() {
+    if (totalPrice > 0) {
+        alert('Terimakasih atas pesanan anda: Rp. ' + (totalPrice * 1000).toLocaleString())
+        reset();
+    } else {
+        alert('Pesan dulu minimal 1 menu')
+    }
+}
+
+function reset(){
+    totalPrice = 0
+    cart = []
+       document.getElementById("checkout").innerHTML = "Rp. 0"
+       document.getElementById("qty")
+
+       for (let i = 0; i < menus.length; i++) {
+        cart.push([0, 0])
+    }
+    
+    let menuArea = "";
+    
+    for (let i = 0; i < menus.length; i++) {
+        const e = menus[i];
+        let menuPrices = "";
+    
+        for (let j = 0; j < e.prices.length; j++) {
+            const f = e.prices[j];
+            menuPrices += `
+                <div class="menu-price-row">
+                    <span class="price-description">${f.label}</span>
+                    <div class="price-and-qty">
+                        <span class="price">${f.price}</span>
+                        <button onclick="substractQty(${i},${j})">
+                            <img src="assets/circle-minus.png" width="16" height="16" alt="minus-circle" />
+                        </button>
+                        <span class="qty" id="qty${i}${j}">0</span>
+                        <button onclick="addQty(${i},${j})">
+                            <img src="assets/circle-plus.png" width="16" height="16" alt="plus-circle" />
+                        </button>
+                    </div>
+                </div>
+            `;
+        }
+    
+        menuArea += `
+            <div class="menu-tile">
+                <div class="menu-photo">
+                    <img src="${e.photoUrl}" alt="${e.title}">
+                </div>
+                <div class="menu-tile-name">${e.title}</div>
+                <div class="menu-tile-description">${e.description}</div>
+            ${menuPrices}
+            </div>
+        `
+    }
+    document.getElementById("menu-area").innerHTML = menuArea;
 }
