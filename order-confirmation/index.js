cb7750732697fd7f1e2865b4afa24713b7489351
CGeH1 <cliffton.gh2008@gmail.com>
2025-05-15 16:00:45 +0700

    display price label

--- order-confirmation/index.js
+++ order-confirmation/index.js
@@ -2,18 +2,20 @@ const params = new URLSearchParams(window.location.search);
 const totalPrice = parseInt(params.get('total-price'));
 const cart = JSON.parse(params.get('cart'));
 
-let content ='';
+let content = '';
 
 content += `<div>Terimakasih atas pesanan anda: Rp ${(totalPrice * 1000).toLocaleString()}</div>`;
 
 for (let i = 0; i < cart.length; i++) {
     const e = cart[i];
-    
-    if(e[0]>1 || e[1] > 1) {
-        content += `<div>Menu ${menus[i].title}: ${e} </div>`;
+
+    if (e[0] > 0) {
+        content += `<div>${menus[i].title} (${menus[i].prices[0].label}): ${e[0]}</div>`;
     }
 
-
+    if (e[1] > 0) {
+        content += `<div>${menus[i].title} (${menus[i].prices[1].label}): ${e[1]}</div>`;
+    }
 }
 
 document.getElementById('outer').innerHTML = content
